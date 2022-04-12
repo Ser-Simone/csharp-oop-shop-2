8a2a919eeec4ebc6c3810eef163bc3ae32d753d8
Simone Serpi <simoneserpi@hotmail.it>
2022-04-12 14:18:42 +0200

    Aggiungere i file di progetto.

---
+++ Csharp Shop 2/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Csharp_Shop
+{
+    internal class Prodotto
+    {
+
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+
+
+
+        public Prodotto(string nome, string descrizione, double prezzo)
+        {
+            
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+           
+        }
+
+        public double getCodiceProdotto()
+        {
+            double codice;
+            Random random = new Random();
+            codice = random.Next(1000, 100000000);
+            return codice;
+        }
+
+        public double ivaProdotto()
+        {
+            double iva;
+            iva = prezzo * 1.22;
+            return iva;
+        }
+
+        public void StampaProdotto()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            Console.WriteLine("Il nome del prodotto è: " + nome);
+            Console.WriteLine("Descrizione del prodotto " + descrizione);
+            Console.WriteLine("Il prezzo è: " + prezzo + " €");
+            Console.WriteLine("Il prezzo compreso di iva è: " + ivaProdotto());
+            Console.WriteLine("Il codice del prodotto è: " + nome + getCodiceProdotto());
+        }
+
+    }
+}
---
+++ Csharp Shop 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Csharp_Shop;
+
+
+Prodotto shop = new Prodotto("Pasta", "spaghetti", 1);
+Console.WriteLine("------- PRODOTTO 1--------");
+shop.StampaProdotto();
+
+
+Prodotto shop2 = new Prodotto("Marmellata", "marmellata di fichi", 3);
+Console.WriteLine("------- PRODOTTO 2--------");
+shop2.StampaProdotto();
+
+Prodotto shop3 = new Prodotto("Pizza", "Napoli", 4.50);
+Console.WriteLine("------- PRODOTTO 3--------");
+shop3.StampaProdotto();
